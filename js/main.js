5292e7a1777b55d7f546a4b30bd3e8729a851a3f
Duchaine Kouassi <kouassi.duchaine@gmail.com>
2020-09-02 17:08:07 +0200

    |ADD| - CLICK -

--- js/main.js
+++ js/main.js
@@ -1,79 +1,74 @@
 // try with table
-
-const imgCard = [
-  "img/palpatineCard.png",
-  "img/luckeCard.png",
-  "img/maulCard.png",
-  "img/yodaCard.png",
-  "img/vadorCard.png",
-  "img/obiCard.png",
-  "img/palpatineCard.png",
-  "img/luckeCard.png",
-  "img/maulCard.png",
-  "img/yodaCard.png",
-  "img/vadorCard.png",
-  "img/obiCard.png"
+const imgCard = [{
+    palpatine: "img/palpatineCard.png",
+    luck: "img/luckeCard.png",
+    maul: "img/maulCard.png",
+    yoda: "img/yodaCard.png",
+    vador: "img/vadorCard.png",
+    obiOne: "img/obiCard.png"
+  },
+  {
+    paaaalpatine: "img/palpatineCard.png",
+    luck: "img/luckeCard.png",
+    maul: "img/maulCard.png",
+    yoda: "img/yodaCard.png",
+    vador: "img/vadorCard.png",
+    obiOne: "img/obiCard.png"
+  },
 ]
-
-for (let c = 0; c < imgCard.length; c++) {
-  const element = imgCard[c];
-
-}
-
-console.log(imgCard)
-
-let imgs = document.getElementsByClassName("img");
-
-for (const img of imgs) {
-  console.log(img)
-  img.addEventListener('click', function () {
-
-    const deck = imgCard[Math.floor(Math.random() * (11)) + 1];
-    console.log(deck)
-    if (deck > 1) {
-      console.log(deck > imgCard[0])
-      this.src = imgCard[0]
-    } else if (deck === imgCard[1]) {
-      this.src = imgCard[1]
-    } else if (deck === imgCard[2]) {
-      this.src = "img/obiCard.png"
-    } else if (deck === imgCard[3]) {
-      this.src = imgCard[3]
-    } else {
-      this.src = imgCard[4]
+const arrayCard = () => {
+  for (const deckL of imgCard) {
+    for (const i in deckL) {
+      const elem = deckL[i];
+      console.log(elem)
+      return elem
     }
-  })
+  }
 }
 
+const deckList = arrayCard()
 
+console.log(deckList)
+let imgs = document.getElementsByClassName("img");
+// console.log(imgs)
 
+for (let i = 0; i < imgs.length; i++) {
+  let img = imgs[i];
+  console.log(imgs[i])
+  img.addEventListener('click', function () {
+    if (imgCard[i] === imgCard[0]) {
+      img.src = imgCard[0]
+    } else if (imgCard[i] === imgCard[1]) {
+      img.src = imgCard[1]
+    } else if (imgCard[i] === imgCard[2]) {
+      img.src = imgCard[2]
+    } else if (imgCard[i] === imgCard[3]) {
+      img.src = imgCard[3]
+    } else if (imgCard[i] === imgCard[4]) {
+      img.src = imgCard[4]
+    } else if (imgCard[i] === imgCard[5]) {
+      img.src = imgCard[5]
+    }
+  });
+}
 
-
-
-
-
-
-
-
-// for (let i = 0; i < myImgs.length; i++) {
-//   const imgList = myImgs[i];
-//   imgList.addEventListener('click', function () {
-//     if (imgList === myImgs[0]) {
-//       this.src = myImgs[1]
-//     } else if (imgList === img[1]) {
-//       this.src = "https://nsa40.casimages.com/img/2020/09/01/20090109364996352.png"
-//     } else if (imgList === img[2]) {
-//       this.src = "https://nsa40.casimages.com/img/2020/09/01//200901095641415463.png"
-//     } else if (imgList === img[3]) {
-//       this.src = "https://nsa40.casimages.com/img/2020/09/01//200901100800480275.png"
-//     } else if (imgList === img[5]) {
-//       this.src = "https://nsa40.casimages.com/img/2020/09/01//20090110242914684.png"
-//     } else {
-//       this.src = "https://nsa40.casimages.com/img/2020/09/01//200901102309956810.png"
-//     }
-//   })
+// -- -- -- -- -- --valide-- -- -- --
+// if (deckCard == imgCard[1] && [7]) {
+//   this.src = imgCard[1]
+// } else if (deckCard == imgCard[2] && [8]) {
+//   this.src = imgCard[2]
+// } else if (deckCard == imgCard[3] && [9]) {
+//   this.src = imgCard[3]
+// } else if (deckCard == imgCard[4] && [10]) {
+//   this.src = imgCard[4]
+// } else if (deckCard == imgCard[0] && [6]) {
+//   this.src = imgCard[0]
+// } else if (deckCard == imgCard[5] && [11]) {
+//   this.src = imgCard[5]
 // }
-
+// // console.log(deckCard)
+// })
+// }
 
 
 //VALIDE
